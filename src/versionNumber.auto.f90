7faca91e45df91458a31289f919f0674503784d2
Max <mmaahn@meteo.uni-koeln.de>
2011-08-12 17:08:11 +0200

    trying to clean the mess...

--- src/versionNumber.auto.f90
+++
@@ -1,8 +0,0 @@
-!edit in makefile only!
-subroutine versionNumber(gitVersion,gitHash)
-implicit none
-character(40), intent(out) ::gitVersion,gitHash
-gitVersion = 'gitVersion'
-gitHash = 'gitHash'
-return
-end subroutine versionNumber
