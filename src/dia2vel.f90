a91d888efe732adf773ce6c2f07e77a136bb45fa
Max <mmaahn@meteo.uni-koeln.de>
2013-04-16 15:54:43 +0200

    new dia2vel relation based on Heymsgield and Westbrook , 2010

--- src/dia2vel.f90
+++ src/dia2vel.f90
@@ -22,8 +22,86 @@ module dia2vel
 
 
   contains
-  ! 
-  ! 
+
+  subroutine dia2vel_heymsfield10_particles &
+    (errorstatus,&     	! out
+    nDia,& 		!in
+    diaSpec_SI,&	!in
+    rho_air_SI,&	!in
+    my_SI,&		!in
+    mass_size_a_SI,&	!in
+    mass_size_b,&	!in
+    area_size_a_SI,&	!in
+    area_size_b,&	!in
+    velSpec)		!out
+
+      !in
+      !nDia: no of diameters
+      !diaSpec_SI = diameter spectrum [m]
+      !rho_air_SI = density of air [kg/m³]
+      !my_SI = kinematic viscosity of air [m²/s]
+      !mass_size_a_SI,mass_size_b parameters of mass size relation m = a*D_max^b [SI]
+      !area_size_a_SI,area_size_b parameters of mass area relation A = a*D_max^b [SI]
+      !out
+      !velSpec: velocity spectrum [m/s]
+
+      !Heymsfield, A. J. & Westbrook, C. D. Advances in the Estimation of Ice Particle Fall Speeds
+      !Using Laboratory and Field Measurements. Journal of the Atmospheric Sciences 67, 2469–2482 (2010).
+
+      ! equations 9-11
+
+
+      ! Modules used:
+      use settings, only: verbose
+      use kinds
+      use constants
+      use report_module
+      implicit none
+
+      integer, intent(in) :: nDia
+      real(kind=dbl), intent(in), dimension(ndia)::diaSpec_SI
+      real(kind=dbl), intent(in) :: rho_air_SI, my_SI,mass_size_a_SI,mass_size_b,&
+      area_size_a_SI,area_size_b
+      real(kind=dbl), dimension(ndia), intent(out) :: velSpec
+      real(kind=dbl), dimension(ndia)::mass,area, area_proj, Xstar, Re
+      real(kind=dbl) ::k, delta_0, C_0, eta
+
+
+      integer(kind=long), intent(out) :: errorstatus
+      integer(kind=long) :: err = 0
+      character(len=80) :: msg
+      character(len=33) :: nameOfRoutine = 'dia2vel_khvorostyanov01_particles'
+
+
+      if (verbose >= 2) call report(info,'Start of ', nameOfRoutine)
+  
+      ! no check for baundaries due to different possible particle types
+      err = success
+
+      k = 0.5d0 !defined in the paper
+      delta_0 = 8.0d0
+      C_0 = 0.35d0
+
+      mass = mass_size_a_SI * diaSpec_SI**mass_size_b
+      area = area_size_a_SI * diaSpec_SI**area_size_b
+      area_proj = area/((pi/4.d0)*diaSpec_SI**2)
+      eta = my_SI * rho_air_SI !now dynamic viscosity
+
+      Xstar = 8.d0 * rho_air_SI * mass * g / (pi * area_proj**(1.d0-k) * eta**2) !eq 9
+      Re = delta_0**2/4.d0 * ((1.d0+((4.d0*sqrt(Xstar))/(delta_0**2*sqrt(C_0))))**0.5 - 1 )**2 !eq10
+     
+      velSpec = eta * Re /(rho_air_SI*diaSpec_SI)
+
+      if (verbose .gt. 1) print*, 'Exiting dia2vel_khvorostyanov01_particles in dia2vel.f90'
+
+      errorstatus = err
+      if (verbose >= 2) call report(info,'End of ', nameOfRoutine)
+
+      return
+
+  end subroutine dia2vel_heymsfield10_particles
+
+
   subroutine dia2vel_khvorostyanov01_particles &
     (errorstatus,&     	! out
     nDia,& 		!in
