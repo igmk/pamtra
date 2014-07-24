b65933f158343e0f66388469c53a28a87ab2ace7
Emiliano Orlandi <eorlandi@meteo.uni-koeln.de>
2014-07-24 10:53:10 +0200

    minor changes for the output of the check_print routine

--- src/check_print.f90
+++ src/check_print.f90
@@ -16,24 +16,23 @@ subroutine check_print()
   
 
      write(112,'(a9,i5,i5,i5,i5,i5)')"x y z f h",i_x,i_y, i_z, i_f, i_h
+     write(112,'(a10,a10,5x,i5,4(3x,f20.10))')hydro_name,dist_name,moment_in,p_1,p_2,p_3,p_4
 
-  if (ABS(am_b-q_h)/q_h*100. .gt. 3) &
-     write(112,'(a10,a10,5x,i5,4(5x,f20.10))')hydro_name,dist_name,moment_in,p_1,p_2,p_3,p_4
 ! total number concentration
   if (moment_in == 1 .or. moment_in == 12 .or. moment_in == 13) then
-     write(112,'(2(a10,5x,f20.10,5x),a10,5x,f20.10,a2)')'m_0',m_0,'n_tot',n_tot,'diff',(m_0-n_tot)/n_tot*100.,' %'
-!   else 
-!      write(112,'(2(a10,5x,f20.10,5x),a10,5x,f20.10,a2)')'m_0',m_0,'n_tot',p_1,'diff',(m_0-p_1)/p_1*100.,' %'
+    if (ABS(m_0-n_tot)/n_tot*100. >= 3) &
+     write(112,'(2(a10,5x,f20.10,5x),a12,5x,f20.10,a2)')'m_0',m_0,'n_tot [m-3]',n_tot,'diff[%]',(m_0-n_tot)/n_tot*100.,' %'
   endif
 ! effective radius
   if (moment_in == 2 .or. moment_in == 12 .or. moment_in == 23) then
-     write(112,'(2(a10,5x,f20.10,5x),a10,5x,f20.10,a2)')'m_32',m_32,'r_eff',r_eff,'diff',(m_32-r_eff)/r_eff*100.,' %'
-!   else 
-!      write(112,'(2(a10,5x,f20.10,5x),a10,5x,f20.10,a2)')'m_32',m_32,'r_eff',p_2,'diff',(m_32-p_2)/p_2*100.,' %'
+     if (ABS(m_32-r_eff)/r_eff*100. >= 3) &
+      write(112,'(2(a10,5x,f20.10,5x),a12,5x,f20.10,a2)')'m_32',m_32,'r_eff [m]',r_eff,'diff[%]',(m_32-r_eff)/r_eff*100.,' %'
   endif
 ! total mass concentration
+  if (moment_in == 3 .or. moment_in == 13 .or. moment_in == 23) then
      if (ABS(am_b-q_h)/q_h*100. .gt. 3) &
-     write(112,'(2(a10,5x,f20.10,5x),a20,5x,f20.10,a2)')'calc',am_b,'in',q_h,'(calc-inp)/in',(am_b-q_h)/q_h*100.,' %'
+      write(112,'(2(a10,5x,f20.10,5x),a12,5x,f20.10,a2)')'m_3',am_b,'mass[kg/m3]',q_h,'diff[%]',(am_b-q_h)/q_h*100.,' %'
+  endif
 
   close(112)
   return
