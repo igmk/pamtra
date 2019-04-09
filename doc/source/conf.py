ced7e27ea91bd055395e24c2284e1e017db2efa2
Maximilian Maahn <maximilian.maahn@colorado.edu>
2019-04-09 16:51:16 +0200

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'nbsphinx',
+#     'nbsphinx',
 ]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
